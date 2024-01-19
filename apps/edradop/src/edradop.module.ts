3605929016434198e855bfbdd88138c9875425a6
toneyavuz <toneyavuz@gmail.com>
2024-01-20 03:44:22 +0700

    secure packages.

--- apps/edradop/src/edradop.module.ts
+++ apps/edradop/src/edradop.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { EdradopController } from './edradop.controller';
 import { EdradopService } from './edradop.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [EdradopController],
   providers: [EdradopService],
 })
--- apps/edradop/src/main.ts
+++ apps/edradop/src/main.ts
@@ -1,19 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { EdradopModule } from './edradop.module';
 import { EDRADOP, EDRADOP_DEFAULT_PORT } from '@edd/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
+import { EdradopModule } from './edradop.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(EdradopModule);
+  const app = await NestFactory.create(EdradopModule, { cors: true });
   const config = app.get(ConfigService);
   const port = config.get(EDRADOP) || EDRADOP_DEFAULT_PORT;
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(csurf());
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
